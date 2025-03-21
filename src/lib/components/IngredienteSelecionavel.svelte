1000103e3a4ad2d799b17139a8242250fb579602
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-03-21 14:31:10 -0300

    first commit

---
+++ src/lib/components/IngredienteSelecionavel.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import Tag from "./Tag.svelte";
+
+  const dispatch = createEventDispatcher<{
+    adicionarIngrediente: string;
+    removerIngrediente: string;
+  }>();
+  export let ingrediente: string;
+  export let selecionado = false;
+  function clicado() {
+    selecionado = !selecionado;
+
+    if (selecionado) {
+      dispatch("adicionarIngrediente", ingrediente);
+    } else {
+      dispatch("removerIngrediente", ingrediente);
+    }
+  }
+</script>
+
+<button on:click={clicado}>
+  <Tag ativa={selecionado}>
+    {ingrediente}
+  </Tag>
+</button>
+
+<style>
+  button {
+    cursor: pointer;
+  }
+</style>
---
+++ src/routes/receitas/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>Receitas!!!</title>
+</svelte:head>
+
+<div>
+  <h1>Receitas</h1>
+  <button>
+    <a href="/">Voltar a página principal ! </a>
+  </button>
+</div>
